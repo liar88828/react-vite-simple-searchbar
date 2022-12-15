427dbd982ab0d4b0e15eeb409211840c34a3b39a
liar780@gmail.com <liar780@gmail.com>
2022-12-15 19:10:58 +0700

    handleFilter

--- src/Component/SearchBar.jsx
+++ src/Component/SearchBar.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 import SearchIcon from '@mui/icons-material/Search';
 
 
 export const SearchBar = ({ placeholder, data }) => {
+
+  const [filterData, setfilterData] = useState([]);
+
+  const handleFilter = e => {
+    const searchWord = e.target.value;
+    const newFilter = data.filter(value => {
+      return value.title.includes(searchWord);
+    });
+
+    setfilterData(newFilter);
+  };
+
   return (
     <div className='search'>
       <h1>Search
       </h1>
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} />
-        <div className="searchIcon">
+        <input type="text" placeholder={placeholder} onChange={handleFilter} />
+        <div className="searchIcon" >
           <SearchIcon />
         </div>
       </div>
-      <div className="dataResult">
-{data.map((value,key)=>{
-  return <div>{value.title}</div>
-})}
+      {
+        filterData.length != 0 && (
 
 
-      </div>
+          <div className="dataResult">
+            {filterData.map((value, key) =>; {
+              return <a
+                key={key}
+                className='dataItem'
+                href={value.link}
+                target='_blank'
+              >{value.title}</a>;
+            })}
+
+
+          </div>
+        )
+      }
+
     </div>
   );
 };
