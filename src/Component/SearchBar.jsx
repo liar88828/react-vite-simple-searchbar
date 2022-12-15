7a43a6ce79a041c351c754bf4032d8aaab81aa81
liar780@gmail.com <liar780@gmail.com>
2022-12-15 18:43:01 +0700

    add icpn and style

---
+++ src/Component/SearchBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './SearchBar.css';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+export const SearchBar = ({ placeholder, data: { } }) => {
+  return (
+    <div className='search'>
+      <h1>Search
+      </h1>
+      <div className="searchInputs">
+        <input type="text" placeholder={placeholder} />
+        <div className="searchIcon">
+          <SearchIcon />
+        </div>
+      </div>
+      {/* <div className="dataResult"></div> */}
+    </div>
+  );
+};
